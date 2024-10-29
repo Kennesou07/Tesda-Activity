fbbb4f4bcc92ce22c4580a9dc7b58ee5f140c77c
John Kenneth Biscocho <145242496+Kennesou07@users.noreply.github.com>
2024-10-29 17:39:34 +0800

    Update DoubleTheElementValue.java

--- Group Activity/DoubleTheElementValue.java
+++ Group Activity/DoubleTheElementValue.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 
 public class DoubleTheElementValue {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int arraySize = scanner.nextInt();
+        int[] arrayValues = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            System.out.print("Enter array "+(i+1)+": ");
+            arrayValues[i] = scanner.nextInt();
+        }
+        System.out.println("Original input array: " + Arrays.toString(arrayValues));
         System.out.println("Double of input array: " + Arrays.toString(DoubleElement(arrayValues)));
     }
     private static int[] DoubleElement(int[] value){
